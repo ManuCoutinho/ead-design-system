b0cee702faad7d86968b457bd4c4641601216bf6
Manu Coutinho <gdn.sasha@gmail.com>
2025-03-10 20:35:34 -0300

    :tada:

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.(spec|test).ts'],
+  collectCoverage: true,
+  moduleNameMapper: {
+    '^@/tests/(.*)': '<rootDir>/tests/$1',
+    '^@/(.*)': '<rootDir>/src/$1'
+
+  },
+  coveragePathIgnorePatterns: ['dist'],
+
+
+}
---
+++ tests/index.spec.ts
@@ -0,0 +1,6 @@
+import { dummy } from "@/index";
+
+it('should return a correctly dummy function', () => {
+  expect(dummy(2, 3)).toBe(5)
+
+});
